556183ea612c8b78fc28f76c3330cd20e64976c2
Ant Man <34754995+aaronamendez@users.noreply.github.com>
2022-01-15 01:27:06 -0500

    Fixed validation glitch on client page.

--- client/src/validations/schema.js
+++ client/src/validations/schema.js
@@ -4,7 +4,7 @@ export const addClientValidations = yup.object().shape({
 	clientName: yup
 		.string()
 		.required('client name is required')
-		.min(5, 'client name must be at least 2 characters'),
+		.min(2, 'client name must be at least 2 characters'),
 	address: yup
 		.string()
 		.required('address is required')
